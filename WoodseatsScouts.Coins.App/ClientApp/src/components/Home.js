08581043451bf0bcb06731f717a6b30fbe595fdb
archvega <archvega>
2023-04-16 20:07:23 +0100

    no message

--- WoodseatsScouts.Coins.App/ClientApp/src/components/Home.js
+++ WoodseatsScouts.Coins.App/ClientApp/src/components/Home.js
@@ -1,26 +1,74 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 export class Home extends Component {
-  static displayName = Home.name;
+    static displayName = Home.name;
 
-  render() {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h3>COINS</h3>
+
+                <div className="row">
+                    <div className="col-6 offset-3">
+                        <input id="scout-code-textbox"
+                               type="text"
+                               className="form-control"
+                               placeholder="Click here and scan scout code"
+                               autoComplete="off"/>
+                    </div>
+                    <div className="col-3">
+                        <button className="btn btn-primary">&#128247;</button>
+                    </div>
+
+                    <div id="modal" title="Barcode scanner">
+                        <span className="found"></span>
+                        <div id="interactive" className="viewport"></div>
+                    </div>
+                </div>
+
+                <hr/>
+
+                <div className="col-6">
+                    <h4 data-bind="text: scoutName"></h4>
+                    <span id="scoutTroopNumberAndSection"></span>
+                    <img id="scout-photo" data-bind="attr: { src:scoutPhotoPath }"/>
+                </div>
+
+                <div className="col-6">
+                    <h4>Scan</h4>
+
+                    <div className="row">
+                        <div className="col-12">
+                            <input id="coin-textbox"
+                                   type="text"
+                                   data-bind="textInput: lastScannedCoinCode, valueUpdate: 'keyup', event: { keypress: onCoinCodeFieldKepPressed }, enable: scanCoinFieldEnabled"
+                                   className="form-control"
+                                   placeholder="Click here and scan a new coin"
+                                   autoComplete="off"/>
+                        </div>
+                    </div>
+
+                    <div id="scanned-coins-div" data-bind="foreach: scannedCoins">
+                        <span className="tag label label-info">
+                            <span data-bind="text:displayText"></span>
+                            <span id="remove-score">
+                                <i data-bind="click: function() { $parent.confirmRemovePoint($data) } "
+                                   className="remove">&#10060;</i>
+                            </span>
+                        </span>
+                    </div>
+
+                    <span>
+                        <b>Total:&nbsp;</b>
+                        <span data-bind="text: totalPoints"></span>
+                    </span>
+                    <hr/>
+
+                    <button className="btn btn-success float-end"
+                            data-bind="click: function() { confirmSubmit() }">Finished
+                    </button>
+                </div>
+            </div>
+        );
+    }
 }
