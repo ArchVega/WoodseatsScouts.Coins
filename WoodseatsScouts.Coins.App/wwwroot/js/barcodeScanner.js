810deb7ddaa735c413f0c4e4cf18060a2c84de12
archvega <archvega>
2023-04-16 15:44:12 +0100

    no message

--- WoodseatsScouts.Coins.App/wwwroot/js/barcodeScanner.js
+++ WoodseatsScouts.Coins.App/wwwroot/js/barcodeScanner.js
@@ -38,6 +38,7 @@
             }
         },
         applySettingsVisibility: function(setting, capability) {
+            // depending on type of capability
             if (typeof capability === 'boolean') {
                 var node = document.querySelector('input[name="settings_' + setting + '"]');
                 if (node) {
@@ -63,6 +64,10 @@
                         return text.length > 30 ? text.substr(0, 30) : text;
                     }
                     var $deviceSelection = document.getElementById("deviceSelection");
+                    if ($deviceSelection == null) {
+                        return;                        
+                    }
+                    
                     while ($deviceSelection.firstChild) {
                         $deviceSelection.removeChild($deviceSelection.firstChild);
                     }
@@ -204,7 +209,7 @@
             frequency: 10,
             decoder: {
                 readers : [{
-                    format: "code_128_reader",
+                    format: "ean_reader",
                     config: {}
                 }]
             },
@@ -215,11 +220,41 @@
 
     App.init();
 
+    Quagga.onProcessed(function(result) {
+        var drawingCtx = Quagga.canvas.ctx.overlay,
+            drawingCanvas = Quagga.canvas.dom.overlay;
 
+        if (result) {
+            if (result.boxes) {
+                drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+                result.boxes.filter(function (box) {
+                    return box !== result.box;
+                }).forEach(function (box) {
+                    Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 2});
+                });
+            }
+
+            if (result.box) {
+                Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "#00F", lineWidth: 2});
+            }
+
+            if (result.codeResult && result.codeResult.code) {
+                Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 3});
+            }
+        }
+    });
 
     Quagga.onDetected(function(result) {
         var code = result.codeResult.code;
-        Quagga.stop();
-        window.location.href="scannerview.php?barcode=" + code;
+
+        if (App.lastResult !== code) {
+            App.lastResult = code;            
+            var $node = null, canvas = Quagga.canvas.dom.image;
+
+            $node = $('<li><div class="thumbnail"><div class="imgWrapper"><img /></div><div class="caption"><h4 class="code"></h4></div></div></li>');
+            $node.find("img").attr("src", canvas.toDataURL());
+            $node.find("h4.code").html(code);
+            $("#result_strip ul.thumbnails").prepend($node);
+        }
     });
 });
