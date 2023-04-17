47a8f494d7cce5d7e5efee0e010688975373e811
archvega <archvega>
2023-04-17 23:06:28 +0100

    no message

---
+++ WoodseatsScouts.Coins.App/ClientApp/src/components/_dev/TestUsersList.js
@@ -0,0 +1,18 @@
+﻿const TestUsersList = ({onSelected}) => {
+    const testUsers = [
+        { name: "Scout A", code: "M013B004" },
+        { name: "Scout B", code: "M013B005" },
+        { name: "Scout C", code: "M013B008" },
+    ]
+    return <>
+        <ul className="list-group list-unstyled">
+            {testUsers.map(testUser => (
+                <li className="list-group-item" style={{cursor: "pointer"}} onClick={() => onSelected(testUser.code)}>
+                        {testUser.name + " - " + testUser.code}
+                </li>                
+            ))}
+        </ul>
+    </>
+}
+
+export default TestUsersList;
--- WoodseatsScouts.Coins.App/ClientApp/src/components/coins/userScanner.js
+++ WoodseatsScouts.Coins.App/ClientApp/src/components/coins/userScanner.js
@@ -1,28 +1,39 @@
 import React, {useContext, useEffect, useState} from "react";
 import {Button, Card, CardBody, CardHeader, Input, InputGroup} from "reactstrap";
 import {AppCameraAvailableContext, AppTestModeContext} from "../../App";
+import TestUsersModal from "../_dev/TestUsersModal";
+import TestUsersList from "../_dev/TestUsersList";
 
 const UserScanner = () => {
+    const [userQRCode, setUserQRCode] = useState("")
     const [user, setUser] = useState({})
     const [appCameraAvailable] = useContext(AppCameraAvailableContext)
     const [appTestMode, setAppTestMode] = useContext(AppTestModeContext);
+    const [testUsersModal, setTestUsersModal] = useState(false);
 
     useEffect(() => {
         const fetchUser = async () => {
-            // const response = await fetch("home/GetScoutInfoFromCode?code=M013B004");
-            // const user = await response.json();
-            // setUser(user);
+            const response = await fetch("home/GetScoutInfoFromCode?code=" + userQRCode);
+            const user = await response.json();
+            setUser(user);
         }
         fetchUser().then();
-    }, [])
+    }, [userQRCode])
+
+    function onScoutCodeTextBoxClicked() {
+        if (appTestMode) {
+            setTestUsersModal(true);
+        }
+    }
 
     return <>
         <div className="col-6 offset-3 mb-2">
             <Card className="text-center">
                 <CardHeader>
-                    {appCameraAvailable
-                        ? <span>Scan a scout's wristband using a barcode reader or click the camera icon to take a picture using the camera.</span>
-                        : <span>Scan a scout's wristband using a barcode reader.</span>
+                    {
+                        appCameraAvailable
+                            ? <span>Scan a scout's wristband using a barcode reader or click the camera icon to take a picture using the camera.</span>
+                            : <span>Scan a scout's wristband using a barcode reader.</span>
                     }
                 </CardHeader>
                 <CardBody>
@@ -31,9 +42,11 @@ const UserScanner = () => {
                                autoComplete="off"
                                placeholder={
                                    appTestMode
-                                       ? "Test mode: click here to select test users" 
+                                       ? "Test mode: click here to select test users"
                                        : "To scan a members QR code with a USB scanner, click here and then scan"}
-                               defaultValue={user.scoutName}/>
+                               defaultValue={user.scoutName}
+                               onClick={onScoutCodeTextBoxClicked}
+                        />
                         {
                             appCameraAvailable
                                 ? <Button className="btn btn-primary">&#128247;</Button>
@@ -48,6 +61,10 @@ const UserScanner = () => {
             <span className="found"></span>
             <div id="interactive" className="viewport"></div>
         </div>
+
+        <TestUsersModal testUsersModal={testUsersModal} setTestUsersModal={setTestUsersModal}>
+            <TestUsersList onSelected={(code) => setUserQRCode(code)}></TestUsersList>
+        </TestUsersModal>
     </>
 }
 
