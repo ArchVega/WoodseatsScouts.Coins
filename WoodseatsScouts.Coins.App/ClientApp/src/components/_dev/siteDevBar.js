7a4d31208a50b5ad2dbfe518ce64a89afc76464c
archvega <archvega>
2023-04-17 19:07:01 +0100

    no message

---
+++ WoodseatsScouts.Coins.App/ClientApp/src/components/_dev/siteDevBar.js
@@ -0,0 +1,40 @@
+﻿import {
+    Collapse, Dropdown,
+    DropdownItem,
+    DropdownMenu,
+    DropdownToggle,
+    Navbar,
+    NavbarBrand, NavbarText,
+    NavLink,
+    UncontrolledDropdown
+} from "reactstrap";
+import {Link} from "react-router-dom";
+import React, {useState} from "react";
+
+const SiteDevBar = ({ direction, ...args }) => {
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
+    const toggle = () => setDropdownOpen((prevState) => !prevState);
+    
+    return <>
+        <header>
+            <Navbar className="navbar-expand-sm navbar-toggleable-sm border-bottom box-shadow mb-3 bg-info" container
+                    dark>
+                <NavbarText className="text-dark">
+                    Developer Bar
+                </NavbarText>
+                <ul className="navbar-nav flex-grow">
+                    <Dropdown isOpen={dropdownOpen} toggle={toggle} direction={direction}>
+                        <DropdownToggle caret>Mode</DropdownToggle>
+                        <DropdownMenu {...args}>
+                            <DropdownItem>Dev</DropdownItem>
+                            <DropdownItem>Release</DropdownItem>                            
+                        </DropdownMenu>
+                    </Dropdown>
+                </ul>
+            </Navbar>
+        </header>
+    </>
+}
+
+export default SiteDevBar;
--- WoodseatsScouts.Coins.App/ClientApp/src/components/common/Layout.js
+++ WoodseatsScouts.Coins.App/ClientApp/src/components/common/Layout.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
-import { Container } from 'reactstrap';
-import { NavMenu } from './NavMenu';
+import React, {Component} from 'react';
+import {Container} from 'reactstrap';
+import {NavMenu} from './NavMenu';
+import SiteDevBar from "../_dev/siteDevBar";
 
 export class Layout extends Component {
-  static displayName = Layout.name;
+    static displayName = Layout.name;
 
-  render() {
-    return (
-      <div>
-        <NavMenu />
-        <Container tag="main">
-          {this.props.children}
-        </Container>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <SiteDevBar/>
+                <NavMenu/>
+                <Container tag="main">
+                    {this.props.children}
+                </Container>
+            </div>
+        );
+    }
 }
