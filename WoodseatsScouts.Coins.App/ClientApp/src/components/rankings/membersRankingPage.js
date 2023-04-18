1171eafaab0e9b7836e3c401217af931068e671e
archvega <archvega>
2023-04-18 22:46:25 +0100

    no message

--- WoodseatsScouts.Coins.App/ClientApp/src/components/rankings/membersRankingPage.js
+++ WoodseatsScouts.Coins.App/ClientApp/src/components/rankings/membersRankingPage.js
@@ -1,6 +1,67 @@
-﻿const MembersRankingPage = () => {
+﻿import React from "react";
+import DataTableComponent, {Styles} from "../common/DataTableComponent";
+
+const MembersRankingPage = () => {
+    const columns = React.useMemo(
+        () => [
+            {
+                Header: 'Name',
+                columns: [
+                    {
+                        Header: 'First Name',
+                        accessor: 'firstName',
+                    },
+                    {
+                        Header: 'Last Name',
+                        accessor: 'lastName',
+                    },
+                ],
+            },
+            {
+                Header: 'Info',
+                columns: [
+                    {
+                        Header: 'Age',
+                        accessor: 'age',
+                    },
+                    {
+                        Header: 'Visits',
+                        accessor: 'visits',
+                    },
+                    {
+                        Header: 'Status',
+                        accessor: 'status',
+                    },
+                    {
+                        Header: 'Profile Progress',
+                        accessor: 'progress',
+                    },
+                ],
+            },
+        ],
+        []
+    )
+
+    function makeData() {
+        return [
+            {
+                firstName: "Boom",
+                lastName: "Shakalaka",
+                age: 100,
+                visits: 3,
+                progress: 93,
+                status: 'relationship is complicated'
+            }
+        ]
+        
+    }
+
+    const data = React.useMemo(() => makeData(2000), [])
     return <>
-    <h3>Member rankings</h3>
+        <h3>Member rankings</h3>
+        <Styles>
+            <DataTableComponent columns={columns} data={data}/>
+        </Styles>
     </>
 }
 
