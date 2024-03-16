0df4d10d9a5f510bccd52236137fb8c9b2ed10e8
dev <dev@localhost>
2024-03-16 11:17:42 +0000

    na

--- Src/WoodseatsScouts.Coins.Api/Config/AppStartupValidator.cs
+++ Src/WoodseatsScouts.Coins.Api/Config/AppStartupValidator.cs
@@ -1,11 +1,14 @@
 // dotcover disable
+
+using Microsoft.Extensions.Options;
+
 namespace WoodseatsScouts.Coins.Api.Config;
 
 public static class AppStartupValidator
 {
     public static void Validate(WebApplication app)
     {
-        var appSettings = app.Services.GetService<AppSettings>();
+        var appSettings = app.Services.GetService<IOptions<AppSettings>>()!.Value;
         if (string.IsNullOrWhiteSpace(appSettings?.ContentRootDirectory))
         {
             throw new ApplicationException("AppSettings.Json error: ContentRootDirectory is null or empty");
