c9b6ce0ffe1f50c9842cef1a1395922f49b5454c
ArchVega <ArchVega>
2026-04-18 12:59:52 +0100

    WIP dashboard-styled fixed viewport

--- Src/woodseatsscouts.coins.web/src/components/common/Layout.tsx
+++ Src/woodseatsscouts.coins.web/src/components/common/Layout.tsx
@@ -1,19 +1,20 @@
-import React, {Component} from 'react';
 import SiteDevBar from "../_dev/siteDevBar.tsx";
 import NavMenu from "../navigation/NavMenu.tsx";
-// import SiteDevBar from "../_dev/siteDevBar";
-// import NavMenu from "../navigation/NavMenu";
 // import {ToastContainer} from "react-toastify";
 // import 'react-toastify/dist/ReactToastify.css';
 
 export default function Layout({children}) {
-    return <div className={"app-shell d-flex flex-column min-vh-100"}>
-        <SiteDevBar />
-        <NavMenu />
-        <div id="site-container" className="container">
-            {children}
-        {/*</Container>*/}
+  return (
+    <div className={"site-container d-flex flex-column"}>
+      <SiteDevBar/>
+      <NavMenu/>
+      <div className="page-container container flex-grow-1">
+        {children}
         {/*<ToastContainer />*/}
-        </div>
+      </div>
+      <footer className="page-footer">
+        Scouts Coin App
+      </footer>
     </div>
+  )
 }
