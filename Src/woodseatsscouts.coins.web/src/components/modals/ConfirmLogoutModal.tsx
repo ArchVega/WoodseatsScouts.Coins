ce3d18e931fe201dc8f63aa6a94810ddc718f590
ArchVega <ArchVega>
2026-04-18 17:49:36 +0100

    WIP scan coins section

---
+++ Src/woodseatsscouts.coins.web/src/components/modals/ConfirmLogoutModal.tsx
@@ -0,0 +1,28 @@
+﻿import {BaseModal} from "./BaseModal.tsx";
+import {Button} from "../common/HtmlControlWrappers.tsx";
+
+export default function ConfirmLogoutModal({showConfirmLogoutModal, setShowConfirmLogoutModal}) {
+  return (
+    <BaseModal id={"app-settings-modal"} title={"Application Settings"} show={showConfirmLogoutModal} onClose={() => {
+      setShowConfirmLogoutModal(false)
+    }}>
+      <div className="row mb-3">
+        <div className="col">
+          <p>Are you sure?</p>
+        </div>
+      </div>
+      <div className="row start-over-modal-buttons">
+        <div className="col">
+          <Button className="btn btn-danger" onClick={() => window.location.reload()} data-testid="button-confirm-start-again">
+            Yes
+          </Button>
+        </div>
+        <div className="col">
+          <Button type="button" className="btn btn-success" onClick={() => setShowConfirmLogoutModal(false)}>
+            No
+          </Button>
+        </div>
+      </div>
+    </BaseModal>
+  )
+}
--- Src/woodseatsscouts.coins.web/src/components/navigation/NavMenu.tsx
+++ Src/woodseatsscouts.coins.web/src/components/navigation/NavMenu.tsx
@@ -8,183 +8,149 @@ import {AppCameraAvailableContext, PageActionMenuAreaContext} from "../../contex
 import SectionNames from "../../pages/home-page/sections/SectionNames.ts";
 import {Button, Image} from "../common/HtmlControlWrappers.tsx";
 import AppSettingsModal from "../modals/AppSettingsModal.tsx";
+import ConfirmLogoutModal from "../modals/ConfirmLogoutModal.tsx";
 
 export default function NavMenu() {
-  const {pageActionMenuAreaAction, setPageActionMenuAreaAction, activeScanningMember, setActiveScanningMember} = useContext(PageActionMenuAreaContext)
+  const {pageActionMenuAreaAction, activeScanningMember} = useContext(PageActionMenuAreaContext)
 
-  const [activeScanningMemberFragment, setActiveScanningMemberFragment] = useState(null);
-  const [pageActionMenuAreaFragment, setPageActionMenuAreaFragment] = useState(null);
-  const [currentPage, setCurrentPage] = useState("");
   const [collapsed, setCollapsed] = useState(true);
   const [appSettingsModal, setAppSettingsModal] = useState(false);
-  const [startAgainModal, setStartAgainModal] = useState(false);
+  const [showConfirmLogoutModal, setShowConfirmLogoutModal] = useState(false);
   const {appCameraAvailable} = useContext(AppCameraAvailableContext)
   const [showNavBarMenu, setShowNavBarMenu] = useState(false)
 
-  function CurrentCoinsPageViewSwitchedToScanMember() {
-    setActiveScanningMemberFragment(null);
-    setPageActionMenuAreaFragment((
-      <Button className={"btn-outline-light text-black bg-white"} style={{width: "230px"}} onClick={() => window.location.reload()}>
-        Not scanning? <br/>Click here and try again.
-      </Button>
-    ))
-  }
-
-  function CurrentCoinsPageViewSwitchedToScanCoins() {
-    // setActiveScanningMemberFragment(
-    //   <div className="d-flex">
-    //     <div className="flex-shrink-0 px-2">
-    //       <Image className="member-image mb-2"
-    //              style={{width: "100px", height: "100px"}}
-    //              src={activeScanningMember && activeScanningMember.hasImage ? Uris.memberPhoto(activeScanningMember.memberId) : "/images/unknown-member-image.png"}></Image>
-    //     </div>
-    //     <div className="flex-fill">
-    //       <div><span className="text-white">Hello,</span>&nbsp;<b className="text-white">{activeScanningMember.firstName}</b></div>
-    //       <div>{activeScanningMember.memberSectionName}, {activeScanningMember.memberTroopName}</div>
-    //     </div>
-    //   </div>
-    // )
-    // setPageActionMenuAreaFragment(
-    // <button type="button"
-    //         style={{top: "18px", right: "34px", width: "230px"}}
-    //         data-testid="button-start-again"
-    //         className="btn btn-outline-light"
-    //         onClick={(e) => {
-    //           setStartAgainModal(true)
-    //           e.stopPropagation()
-    //         }}>
-    //   Not {activeScanningMember.firstName}? <br/> Click here to log out.
-    // </button>
-    // )
-  }
-
-  function CurrentCoinsPageViewSwitchedToHaulSummary() {
-    setActiveScanningMemberFragment(null);
-    setPageActionMenuAreaFragment(null)
-  }
-
-  useEffect(() => {
-    if (pageActionMenuAreaAction === SectionNames.ScanMember) {
-      CurrentCoinsPageViewSwitchedToScanMember();
-    } else if (pageActionMenuAreaAction === SectionNames.ScanCoins) {
-      CurrentCoinsPageViewSwitchedToScanCoins();
-    } else {
-      CurrentCoinsPageViewSwitchedToHaulSummary();
-    }
-  }, [pageActionMenuAreaAction]);
-
   function RenderNavBarSubmenu() {
-    if (showNavBarMenu) {
+    const RenderNavBarSubmenuItems = () => {
+      const MenuNavLink = (text: string, targetPageName: string, targetLocation: string, dataTestId: string) => {
+        return (
+          <NavLink
+            data-testid={dataTestId}
+            className={"scouts-nav-link m-3"}
+            to={targetLocation}>
+            {text}
+          </NavLink>
+        )
+      }
+
+      const RenderAppSettingsGearIcon = () => {
+        if (appCameraAvailable) {
+          return (
+            <span id="app-settings-gear" data-testid="nav-settings-modal" onClick={() => setAppSettingsModal(true)}>
+              ⛭
+            </span>
+          )
+        }
+
+        return null
+      }
+
+      const RenderReturnToMainScreenButton = () => {
+        return (
+          <div>
+            <span data-testid="nav-coins-page" onClick={() => window.location.reload()} className={"scouts-nav-link m-3"}>
+              <div className={"px-3 py-1 scouts-borders-white"}>
+                Return to Main Screen
+              </div>
+            </span>
+            {/*<NavLink data-testid="nav-coins-page" onClick={() => setCurrentPage("coins")} className={"scouts-nav-link m-3"} to="/">*/}
+            {/*  <div className={"px-3 py-1 scouts-borders-white"}>*/}
+            {/*    Return to Main Screen*/}
+            {/*  </div>*/}
+            {/*</NavLink>*/}
+          </div>
+        )
+      }
+
       return (
-        <div className="container-fluid scouts-nav-bar-sub-menu">
-          <div className="container">
-            <div className="d-sm-inline-flex w-100 align-items-center">
-              {RenderMenuItems()}
-            </div>
+        <>
+          {MenuNavLink("Participants", "members", "/members", "nav-members-page")}
+          {MenuNavLink("Secondary Dashboard", "leaderboard-members", "/leaderboard/members", "nav-report-page-leaderboard-members")}
+          <div className={"ms-auto"}>
+            {RenderAppSettingsGearIcon()}
+          </div>
+          {RenderReturnToMainScreenButton()}
+        </>
+      )
+    }
+
+    if (!showNavBarMenu) {
+      return null
+    }
+
+    return (
+      <div className="container-fluid scouts-nav-bar-sub-menu">
+        <div className="container">
+          <div className="d-sm-inline-flex w-100 align-items-center">
+            {RenderNavBarSubmenuItems()}
           </div>
         </div>
-      )
-    }
-
-    return null
-  }
-
-  function MenuNavLink(text: string, targetPageName: string, targetLocation: string, dataTestId: string) {
-    return (
-      <NavLink
-        data-testid={dataTestId}
-        onClick={() => setCurrentPage(targetPageName)}
-        className={"scouts-nav-link m-3"}
-        to={targetLocation}>
-        {text}
-      </NavLink>
-    )
-  }
-
-  function RenderAppSettingsGearIcon() {
-    if (appCameraAvailable) {
-      return (
-        <span id="app-settings-gear" data-testid="nav-settings-modal" onClick={() => setAppSettingsModal(true)}>
-          ⛭
-        </span>
-      )
-    }
-
-    return null
-  }
-
-  function RenderReturnToMainScreenButton() {
-    return (
-      <div>
-        <NavLink data-testid="nav-coins-page" onClick={() => setCurrentPage("coins")} className={"scouts-nav-link m-3"} to="/">
-          <div className={"px-3 py-1 scouts-borders-white"}>
-            Return to Main Screen
-          </div>
-        </NavLink>
       </div>
     )
   }
 
-  function RenderMenuItems() {
-    return (
-      <>
-        {MenuNavLink("Participants", "members", "/members", "nav-members-page")}
-        {MenuNavLink("Secondary Dashboard", "leaderboard-members", "/leaderboard/members", "nav-report-page-leaderboard-members")}
-        <div className={"ms-auto"}>
-          {RenderAppSettingsGearIcon()}
-        </div>
-        {RenderReturnToMainScreenButton()}
-      </>
-    )
+  function RenderLeftSideHeaderSection() {
+    switch (pageActionMenuAreaAction) {
+      case SectionNames.ScanCoins:
+        return (
+          <div className="d-flex">
+            <div className="flex-shrink-0 px-2">
+              <Image className="member-image mb-2"
+                     style={{width: "100px", height: "100px"}}
+                     src={activeScanningMember && activeScanningMember.hasImage ? Uris.memberPhoto(activeScanningMember.memberId) : "/images/unknown-member-image.png"}></Image>
+            </div>
+            <div className="flex-fill">
+              <div><span className="text-white">Hello,</span>&nbsp;<b className="text-white">{activeScanningMember.firstName}</b></div>
+              <div>{activeScanningMember.memberSectionName}, {activeScanningMember.memberTroopName}</div>
+            </div>
+          </div>
+        )
+      default:
+        return null
+    }
+  }
+
+  function RenderRightSideHeaderSection() {
+    switch (pageActionMenuAreaAction) {
+      case SectionNames.ScanMember:
+        return (
+          <Button className={"btn-outline-light text-black bg-white"} style={{width: "230px"}} onClick={() => window.location.reload()}>
+            Not scanning? <br/>Click here and try again.
+          </Button>
+        )
+      case SectionNames.ScanCoins:
+        return (
+          <button type="button"
+                  style={{top: "18px", right: "34px", width: "230px"}}
+                  data-testid="button-start-again"
+                  className="btn btn-outline-light"
+                  onClick={(e) => {
+                    setShowConfirmLogoutModal(true)
+                    e.stopPropagation()
+                  }}>
+            Not {activeScanningMember.firstName}? <br/> Click here to log out.
+          </button>
+        )
+      default:
+        return null
+    }
   }
 
   return (
     <header className={"scouts-nav-bar"}>
       <div className="container navbar navbar-expand-sm navbar-toggleable-sm ng-white box-shadow align-middle" style={{height: "100px"}}>
-        <div className="flex-fill text-start">
-          {pageActionMenuAreaAction === SectionNames.ScanCoins && (
-            <>
-              <div className="d-flex">
-                <div className="flex-shrink-0 px-2">
-                  <Image className="member-image mb-2"
-                         style={{width: "100px", height: "100px"}}
-                         src={activeScanningMember && activeScanningMember.hasImage ? Uris.memberPhoto(activeScanningMember.memberId) : "/images/unknown-member-image.png"}></Image>
-                </div>
-                <div className="flex-fill">
-                  <div><span className="text-white">Hello,</span>&nbsp;<b className="text-white">{activeScanningMember.firstName}</b></div>
-                  <div>{activeScanningMember.memberSectionName}, {activeScanningMember.memberTroopName}</div>
-                </div>
-              </div>
-            </>
-          )}
-          {/*{activeScanningMemberFragment}*/}
+        <div className="flex-fill text-start" style={{backgroundColor: "red"}}>
+          {RenderLeftSideHeaderSection()}
         </div>
         <div className="position-absolute top-50 start-50 translate-middle text-center">
           <Image role="button" src={ScoutsLogo} style={{height: "60px", width: "100%"}} onClick={() => setShowNavBarMenu(!showNavBarMenu)}/>
         </div>
-        <div className="flex-fill text-end">
-          {pageActionMenuAreaAction === SectionNames.ScanCoins && (
-            <>
-              <button type="button"
-                      style={{top: "18px", right: "34px", width: "230px"}}
-                      data-testid="button-start-again"
-                      className="btn btn-outline-light"
-                      onClick={(e) => {
-                        setStartAgainModal(true)
-                        e.stopPropagation()
-                      }}>
-                Not {activeScanningMember.firstName}? <br/> Click here to log out.
-              </button>
-            </>
-          )}
-          {/*{pageActionMenuAreaFragment}*/}
+        <div className="flex-fill text-end" style={{backgroundColor: "blue"}}>
+          {RenderRightSideHeaderSection()}
         </div>
       </div>
       {RenderNavBarSubmenu()}
       <AppSettingsModal appSettingsModal={appSettingsModal} setAppSettingsModal={setAppSettingsModal}></AppSettingsModal>
-      {/*<ConfirmStartAgainModal*/}
-      {/*  modal={startAgainModal}*/}
-      {/*  setModal={setStartAgainModal}></ConfirmStartAgainModal>*/}
+      <ConfirmLogoutModal showConfirmLogoutModal={showConfirmLogoutModal} setShowConfirmLogoutModal={setShowConfirmLogoutModal}></ConfirmLogoutModal>
     </header>
   );
 }
