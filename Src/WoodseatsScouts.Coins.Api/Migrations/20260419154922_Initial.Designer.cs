e3dbdc1f73a264546058d48896686b05bc1dd0b3
ArchVega <ArchVega>
2026-04-19 16:47:28 +0100

    WIP updated db data including randomised names

--- Src/WoodseatsScouts.Coins.Api/Migrations/20260419154922_Initial.Designer.cs
+++ Src/WoodseatsScouts.Coins.Api/Migrations/20260419154922_Initial.Designer.cs
@@ -12,7 +12,7 @@ using WoodseatsScouts.Coins.Api.Data;
 namespace WoodseatsScouts.Coins.Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20260419151505_Initial")]
+    [Migration("20260419154922_Initial")]
     partial class Initial
     {
         /// <inheritdoc />
