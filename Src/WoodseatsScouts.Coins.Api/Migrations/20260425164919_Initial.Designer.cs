cd25634c69c6d5c02651179ebe1d74f13947c5b3
ArchVega <ArchVega>
2026-04-26 09:58:37 +0100

    Prod release candidate

--- Src/WoodseatsScouts.Coins.Api/Migrations/20260425164919_Initial.Designer.cs
+++ Src/WoodseatsScouts.Coins.Api/Migrations/20260425164919_Initial.Designer.cs
@@ -12,7 +12,7 @@ using WoodseatsScouts.Coins.Api.Data;
 namespace WoodseatsScouts.Coins.Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20260425114850_Initial")]
+    [Migration("20260425164919_Initial")]
     partial class Initial
     {
         /// <inheritdoc />
--- Utilities/Deployment/Database/RecreateAzureDatabase.ps1
+++ Utilities/Deployment/Database/RecreateAzureDatabase.ps1
@@ -1,4 +1,6 @@
-. .\Utilities\Deployment\Database\InsertCoinsSqlGenerator.ps1
+. .\Utilities/Deployment/Database/InsertCoinData.ps1
+. .\Utilities/Deployment/Database/InsertMemberData.ps1
+. .\Utilities/Deployment/Database/InsertScoutGroups.ps1
 
 $sqlOutputFile = Join-Path (Get-Location) -ChildPath "Utilities/Deployment/Database/Output/WoodseatsScouts.Coins.sql"
 $removeAllTables = Join-Path (Get-Location) -ChildPath "Utilities/Deployment/Database/RemoveAllTables.sql"
@@ -10,30 +12,42 @@ dotnet ef migrations script -o $sqlOutputFile
 $schemaSql = Get-Content $sqlOutputFile -Raw
 
 $insertData = GetCoinData
+$insertData2 = GetMemberData
+$insertData3 = GetScoutGroups
 
 $output = "
 $(Get-Content $removeAllTables -Raw)
 
-USE [WoodseatsScouts.Coins]
-GO
+--USE [WoodseatsScouts.Coins]
+--GO
 
 $schemaSql
 
-if not exists(select * from sys.database_principals where name = 'ScoutsUser')
-begin
-CREATE USER ScoutsUser FOR LOGIN ScoutsUser WITH DEFAULT_SCHEMA = [WoodseatsScouts.Coins]
-end
+--if not exists(select * from sys.database_principals where name = 'ScoutsUser')
+--begin
+--CREATE USER ScoutsUser FOR LOGIN ScoutsUser WITH DEFAULT_SCHEMA = [WoodseatsScouts.Coins]
+--end
 
-INSERT [dbo].[Sections] ([Code], [Name]) VALUES (N'A', N'Adults')
-INSERT [dbo].[Sections] ([Code], [Name]) VALUES (N'B', N'Beavers')
-INSERT [dbo].[Sections] ([Code], [Name]) VALUES (N'C', N'Cubs')
-INSERT [dbo].[Sections] ([Code], [Name]) VALUES (N'E', N'Explorers')
-INSERT [dbo].[Sections] ([Code], [Name]) VALUES (N'S', N'Scouts')
+INSERT [dbo].[ScoutSections] ([Code], [Name]) VALUES (N'A', N'Adults')
+INSERT [dbo].[ScoutSections] ([Code], [Name]) VALUES (N'B', N'Beavers')
+INSERT [dbo].[ScoutSections] ([Code], [Name]) VALUES (N'C', N'Cubs')
+INSERT [dbo].[ScoutSections] ([Code], [Name]) VALUES (N'E', N'Explorers')
+INSERT [dbo].[ScoutSections] ([Code], [Name]) VALUES (N'S', N'Scouts')
+INSERT [dbo].[ScoutSections] ([Code], [Name]) VALUES (N'Q', N'Squirrels')
 GO
 
+-- coins
 $insertData
+
+-- scoutgroups
+$insertData3
+
+-- members
+$insertData2
 GO
 "
+
+$output = $output.Replace("[WoodseatsScouts.Coins.Development]", "[WoodseatsScouts.Coins.2026]")
 
 Set-Content -Path $sqlOutputFile -Value $output
 
